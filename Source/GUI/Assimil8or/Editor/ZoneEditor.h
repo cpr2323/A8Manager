57f8e824a982ad8046854b98cffb4689bf7fe58c
Chris Roberts <cprandroid@gmail.com>
2023-08-14 17:14:34 -0700

    Added Parameter components to ZoneEditor

--- Source/GUI/Assimil8or/Editor/ZoneEditor.h
+++ Source/GUI/Assimil8or/Editor/ZoneEditor.h
@@ -9,6 +9,25 @@ public:
 private:
     ZoneProperties zoneProperties;
 
+    juce::Label levelOffsetLabel;
+    juce::TextEditor levelOffsetTextEdit; // double
+    juce::Label loopLengthLabel;
+    juce::TextEditor loopLengthTextEdit; // double
+    juce::Label loopStartLabel;
+    juce::TextEditor loopStartTextEdit; // int
+    juce::Label minVoltageLabel;
+    juce::TextEditor minVoltageTextEdit; // double
+    juce::Label pitchOffsetLabel;
+    juce::TextEditor pitchOffsetTextEdit; // double
+    juce::Label sampleStartNameLabel;
+    juce::TextEditor sampleFileNameTextEdit; // filename
+    juce::Label sampleStartLabel;
+    juce::TextEditor sampleStartTextEdit; // int
+    juce::Label sampleEndLabel;
+    juce::TextEditor sampleEndTextEdit; // int
+    juce::Label sideLabel;
+    juce::ToggleButton sideCheckBox; // ?
+
     void levelOffsetDataChanged (double levelOffset);
     void levelOffsetUiChanged (double levelOffset);
     void loopLengthDataChanged (double loopLength);
