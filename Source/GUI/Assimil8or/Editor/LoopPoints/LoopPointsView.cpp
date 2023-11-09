bdc3d9dde20a03c5e9289d55aa624e40c2fa18b1
Chris Roberts <cprandroid@gmail.com>
2023-11-09 15:18:54 -0800

    WIP - initial display of waveform

--- Source/GUI/Assimil8or/Editor/LoopPoints/LoopPointsView.cpp
+++ Source/GUI/Assimil8or/Editor/LoopPoints/LoopPointsView.cpp
@@ -1,17 +1,45 @@
 #include "LoopPointsView.h"
 
+LoopPointsView::LoopPointsView (juce::AudioBuffer<float>& theAudioBuffer)
+    :audioBuffer (theAudioBuffer)
+{
+}
+
+void LoopPointsView::setLoopInfo (int64_t theSampleOffset, int64_t theNumSamples)
+{
+    juce::Logger::outputDebugString ("LoopPointsView::setLoopInfo" + juce::String (theSampleOffset) + ", " + juce::String (theNumSamples));
+    jassert (theSampleOffset + theNumSamples <= audioBuffer.getNumSamples ());
+    sampleOffset = theSampleOffset;
+    numSamples = theNumSamples;
+}
+
 void LoopPointsView::paint (juce::Graphics& g)
 {
-    g.setColour (juce::Colours::black);
     const auto halfWidth { getWidth () / 2 };
     const auto halfHeight { getHeight () / 2 };
-    if (endSamples.size () > 1)
-        for (auto sampleOffset { 0 }; sampleOffset < endSamples.size () - 1 && sampleOffset < halfWidth; ++sampleOffset)
-            g.drawLine (sampleOffset, static_cast<int>(halfHeight - (endSamples [sampleOffset] * getHeight ())), sampleOffset + 1, static_cast<int>(halfHeight - (endSamples [sampleOffset + 1] * getHeight ())));
 
-    if (startSamples.size () > 1)
-        for (auto sampleOffset { 0 }; sampleOffset < startSamples.size () - 1 && sampleOffset < halfWidth; ++sampleOffset)
-            g.drawLine (halfWidth + sampleOffset, static_cast<int>(halfHeight - (startSamples [sampleOffset] * getHeight ())), halfWidth + sampleOffset + 1, static_cast<int>(halfHeight - (startSamples [sampleOffset + 1] * getHeight ())));
+
+    if (numSamples > 4)
+    {
+        juce::dsp::AudioBlock<float> audioBlock { audioBuffer };
+        juce::dsp::AudioBlock<float> loopSamples { audioBlock.getSubBlock (sampleOffset, numSamples) };
+        const auto samplesToDisplay {std::min<int>(numSamples, halfWidth)};
+
+        g.setColour (juce::Colours::black);
+        auto readPtr { loopSamples.getChannelPointer (0) };
+        for (auto sampleCount { 0 }; sampleCount < samplesToDisplay - 1; ++sampleCount)
+        {
+            // draw one line of sample going reverse from middle to left
+            const auto xOffset { halfWidth - sampleCount };
+            const auto sampleIndex { numSamples - sampleCount };
+            g.drawLine (xOffset, static_cast<int>(halfHeight - (readPtr [sampleIndex] * getHeight ())),
+                        xOffset + 1, static_cast<int>(halfHeight - (readPtr [sampleIndex + 1] * getHeight ())));
+
+            // draw one line of sample start going from middle to right
+            g.drawLine (halfWidth + sampleCount, static_cast<int>(halfHeight - (readPtr [sampleCount] * getHeight ())),
+                        halfWidth + sampleCount + 1, static_cast<int>(halfHeight - (readPtr [sampleCount + 1] * getHeight ())));
+        }
+    }
 
     g.drawRect (getLocalBounds ());
     g.fillRect (getWidth () / 2, 0, 1, getHeight ());
--- Source/GUI/Assimil8or/Editor/LoopPoints/LoopPointsView.h
+++ Source/GUI/Assimil8or/Editor/LoopPoints/LoopPointsView.h
@@ -5,9 +5,14 @@
 class LoopPointsView : public juce::Component
 {
 public:
-    std::vector<float> startSamples;
-    std::vector<float> endSamples;
+    LoopPointsView (juce::AudioBuffer<float>& theAudioBuffer);
+
+    void setLoopInfo (int64_t theSampleOffset, int64_t theNumSamples);
 
 private:
+    juce::AudioBuffer<float>& audioBuffer;
+    int64_t sampleOffset { 0 };
+    int64_t numSamples { 0 };
+
     void paint (juce::Graphics& g);
 };
